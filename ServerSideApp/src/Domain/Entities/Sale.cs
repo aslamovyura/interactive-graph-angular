933ac2d62c7193d8ef1ae88d33b48c82c2eb4a8d
Aslamov Yury <aslamov.yura@gmail.com>
2020-07-16 10:59:22 +0300

    feat: add application entities

---
+++ ServerSideApp/src/Domain/Entities/Sale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Single sale entity.
+    /// </summary>
+    public class Sale
+    {
+        /// <summary>
+        /// Sale identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Sale date.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Sale amount, $.
+        /// </summary>
+        public double Amount { get; set; }
+    }
+}
