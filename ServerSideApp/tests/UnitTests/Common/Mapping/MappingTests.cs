85dc2e9800fc17be42736a8673b017c5b9a898d4
Aslamov Yury <aslamov.yura@gmail.com>
2020-07-19 14:10:01 +0300

    test: add mapping tests

---
+++ ServerSideApp/tests/UnitTests/Common/Mapping/MappingTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ServerSideApp.Application.DTO;
+using ServerSideApp.Domain.Entities;
+using System;
+using Xunit;
+
+namespace UnitTests.Common.Mapping
+{
+    public class MappingTests : IClassFixture<MappingTestsFixture>
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests(MappingTestsFixture fixture)
+        {
+            _configuration = fixture.ConfigurationProvider;
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public void Mapper_ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Theory]
+        [InlineData(typeof(Sale), typeof(SaleDTO))]
+        public void Mapper_ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = Activator.CreateInstance(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+    }
+}
